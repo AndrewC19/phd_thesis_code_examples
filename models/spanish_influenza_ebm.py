5f0d788dee0a65572d20c41612cb4a0bd2b3c463
AndrewC19 <agclark2@sheffield.ac.uk>
2022-11-24 14:54:35 +0000

    Spanish Influenza EBM works

--- models/spanish_influenza_ebm.py
+++ models/spanish_influenza_ebm.py
@@ -3,6 +3,7 @@ from scipy.integrate import odeint
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+
 plt.rcParams.update(
     {
         "font.family": "serif",
@@ -13,12 +14,12 @@ plt.rcParams.update(
 )
 
 # Model parameters (at point of initialisation)
-START_MORTALITY_PROB = 0.01
+START_MORTALITY_PROB = 0.01  # GAMMA
 START_RECOVERY_TIME = 2.5
 START_MORTALITY_TIME = 1
-START_TRANSMISSION_PROB = 0.15
+START_TRANSMISSION_PROB = 0.15  # BETA
 START_ENCOUNTER_RATE = 4
-START_INCUBATION_TIME = 3
+START_INCUBATION_TIME = 3  # 1/SIGMA
 
 # Initial compartments
 START_REMOVED = 0
@@ -37,6 +38,7 @@ RESOLUTION = DURATION * 1
 class SpanishInfluenzaEBM:
 
     def __init__(self):
+
         # Disease transmission parameters
         self.mortality_prob: float = START_MORTALITY_PROB
         self.recovery_time: float = START_RECOVERY_TIME
@@ -49,62 +51,135 @@ class SpanishInfluenzaEBM:
         self.time_points: list = np.linspace(0, DURATION, RESOLUTION)
 
     @staticmethod
-    def compute_population(recovered, infectious, incubating, susceptible):
-        return recovered + infectious + incubating + susceptible
+    def compute_population(susceptible, incubating, infected, recovered):
+        """Equation for calculating the current living population.
+
+        This equation computes the total living population, given the
+        distribution of susceptible, incubating, infected, and recovered
+        individuals."""
+        return susceptible + incubating + infected + recovered
 
     def compute_r(self, susceptible, population):
+        """Equation for calculating the current R value in the population.
+
+        This equation computes the current R value, given the number of
+        susceptible individuals and the total number of individuals in the
+        population."""
         return (self.transmission_prob * self.encounter_rate *
                 susceptible) / population
 
-    def update_deceased(self, removed):
-        return self.mortality_prob * removed
+    def dDdt(self, to_remove):
+        """Equation for the derivative of deceased.
 
-    def update_recovered(self, removed):
-        return (1 - self.mortality_prob) * removed
+        This equation computes the change in deceased per time step, given the
+        number of individuals to remove in this time step and the probability of
+         mortality."""
+        return self.mortality_prob * to_remove
 
-    def compute_time_step(self, y, t):
-        recovered, infectious, susceptible, incubating, total_infected = y
+    def dRdt(self, to_remove):
+        """Equation for the derivative of recovered.
+
+        This equation computes the change in recovered per time step, given the
+        number of individuals to remove in this time step and the probability of
+         mortality."""
+        return (1 - self.mortality_prob) * to_remove
+
+    def dRemoved_dt(self, infected):
+        """Equation for the derivative of the number of removed individuals.
+
+        This equation computes the number of individuals to remove per time
+        step, given the number of infected individuals, the probability of
+        mortality, time for mortality, and time for recovery."""
+        return ((self.mortality_prob / self.mortality_time +
+                 (1 - self.mortality_prob) / self.recovery_time) *
+                infected)
+
+    def dInfectious_dt(self, d_removed_dt, incubating):
+        """Equation for the derivative of the number of infectious individuals.
+
+        This equation computes change in infected individuals per time step,
+        given the number of infected and incubating individuals, and the
+        incubation time."""
+        return -d_removed_dt + incubating / self.incubation_time
+
+    @staticmethod
+    def dSdt(r, infected):
+        """Equation for the derivative of the number of susceptible individuals.
+
+        This equation computes the change in the number of susceptible
+        individuals per time step, given the current R number and infected
+        individuals."""
+        return -r * infected
+
+    def dIncubating_dt(self, incubating, r, infected):
+        """Equation for the derivative of the number of incubating individuals.
+
+        This equation computes the change in the number of incubating
+        individuals per time step, given the current number of incubating and
+        infected individuals, incubation time, and R number."""
+        return -incubating / self.incubation_time + r * infected
+
+    def dTotalInfected_df(self, incubating):
+        """Equation for the derivative of the total infected individuals.
+
+        This equation computes the change in the total number of infected
+        individuals per time step, given the current number of incubating
+        individuals and the incubation time."""
+        return incubating / self.incubation_time
+
+    def model(self, states: tuple, t):
+        (susceptible, incubating, infected, recovered,
+         dead, total_infected) = states
 
         # Compute constants
-        population = self.compute_population(recovered, infectious,
-                                             incubating, susceptible)
+        population = self.compute_population(susceptible, incubating,
+                                             infected, recovered)
         r = self.compute_r(susceptible, population)
 
-        # Compute time derivatives
-        d_removed_dt = (((self.mortality_prob / self.mortality_time) +
-                         (1 - self.mortality_prob) / self.recovery_time) *
-                        infectious)
-        d_infectious_dt = -d_removed_dt + (incubating /
-                                           self.incubation_time)
-        d_susceptible_dt = -r * infectious
-        d_incubating_dt = ((-incubating / self.incubation_time) +
-                           r * infectious)
-        d_total_infected_dt = incubating / self.incubation_time
-        return (d_removed_dt, d_infectious_dt, d_susceptible_dt,
-                d_incubating_dt, d_total_infected_dt)
+        # Compute derivatives
+        d_removed_dt = self.dRemoved_dt(infected)
+        d_deceased_dt = self.dDdt(d_removed_dt)
+        d_recovered_dt = self.dRdt(d_removed_dt)
+        d_infectious_dt = self.dInfectious_dt(d_removed_dt, incubating)
+        d_susceptible_dt = self.dSdt(r, infected)
+        d_incubating_dt = self.dIncubating_dt(incubating, r, infected)
+        d_total_infected_dt = self.dTotalInfected_df(incubating)
+
+        # Update the states
+        updates_states = (d_susceptible_dt, d_incubating_dt, d_infectious_dt,
+                          d_recovered_dt, d_deceased_dt, d_total_infected_dt)
+        return updates_states
 
     def solve(self,
-              initial_recovered=START_RECOVERED,
-              initial_infectious=START_INFECTIOUS,
               initial_susceptible=START_SUSCEPTIBLE,
               initial_incubating=START_INCUBATING,
-              initial_total_infected=START_TOTAL_INFECTED):
+              initial_infectious=START_INFECTIOUS,
+              initial_recovered=START_RECOVERED,
+              initial_deceased=START_DECEASED,
+              initial_total_infected=START_TOTAL_INFECTED,
+              plot: bool = True):
+
         # Define the initial states
-        y0 = (initial_recovered, initial_infectious, initial_susceptible,
-              initial_incubating, initial_total_infected)
+        y0 = (initial_susceptible, initial_incubating, initial_infectious,
+              initial_recovered, initial_deceased, initial_total_infected)
 
         # Solve ODE starting from initial states
-        y = odeint(self.compute_time_step,
+        y = odeint(self.model,
                    y0,
-                   self.time_points)
+                   t=self.time_points)
 
         # Store results in a dataframe
-        results = pd.DataFrame({'recovered': y[:, 0],
-                                'infectious': y[:, 1],
-                                'susceptible': y[:, 2],
-                                'incubating': y[:, 3],
-                                'total_infected': y[:, 4]})
+        results = pd.DataFrame({'susceptible': y[:, 0],
+                                'incubating': y[:, 1],
+                                'infectious': y[:, 2],
+                                'recovered': y[:, 3],
+                                'deceased': y[:, 4],
+                                'total_infected': y[:, 5]})
         results.index.names = ['t']
+
+        # Plot results
+        if plot:
+            self.plot(results)
         return results
 
     @staticmethod
@@ -112,6 +187,7 @@ class SpanishInfluenzaEBM:
         xs = results_df.index
         for col in results_df.columns:
             plt.plot(xs, results_df[col], color='red', linewidth=1)
+            plt.ticklabel_format(axis='y', style='sci', scilimits=(1, 4))
             title_str = col.replace('_', ' ')
             title_case_title_str = title_str.title()
             plt.xlabel("Days")
@@ -122,5 +198,3 @@ class SpanishInfluenzaEBM:
 if __name__ == "__main__":
     spanish_influenza_model = SpanishInfluenzaEBM()
     spanish_influenza_df = spanish_influenza_model.solve()
-    print(spanish_influenza_df)
-    spanish_influenza_model.plot(spanish_influenza_df)
